387d459dac56ca5a4aaa1d2a59b7d08f13722e26
Söhnke <soehnkeg@gmail.com>
2024-03-28 17:29:55 +0100

    api for ocr using easyocr

--- main.py
+++ main.py
@@ -1,13 +1,49 @@
-from fastapi import FastAPI
+import base64
+import cv2
+import easyocr
+from flask import Flask, request, jsonify, app
+from PIL import Image
+import io
 
-app = FastAPI()
+reader = easyocr.Reader(['en'], gpu=False)
+app = Flask(__name__)
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.route("/solve", methods=['POST'])
+def solve():
+    print("received request")
+    data = request.get_json()
+    file = data.get('image')
+
+    if not file:
+        return jsonify({'error': 'No image provided'}), 401
+
+    image_bytes = base64.b64decode(file)
+    image = io.BytesIO(image_bytes)
+    
+    try:
+        img = Image.open(image)
+        img.verify()  # Raises an exception if the image is invalid
+    except Exception as e:
+        print("Error decoding image:", e)
+
+    results = reader.readtext(image_bytes, allowlist='0123456789')
+
+    # Prepare the extracted text
+    text = ''
+    for result in results:
+        text += result[1] + ' '
+
+    print(f"text is {text}")
+
+    return jsonify({'text': text})
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.route("/", methods=['GET'])
+def root():
+    print("RECEIVED REQUEST")
+    return jsonify({"message": "Hello World!"})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
